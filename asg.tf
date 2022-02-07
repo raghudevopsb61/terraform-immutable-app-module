b1b253cc8d3f8a395cdf3d9059fc1b508d7972ad
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-07 07:50:03 +0530

    Update terraform-immutable-app-module/

--- asg.tf
+++ asg.tf
@@ -11,7 +11,7 @@ resource "aws_autoscaling_group" "asg" {
 
   tag {
     key                 = "Name"
-    value               = "${var.COMPONENT}-${var.APP_VERSION}"
+    value               = "${var.COMPONENT}-${var.ENV}"
     propagate_at_launch = true
   }
 
