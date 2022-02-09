42b32b2fb6f4280534130ef0450f7d6ab21d3f06
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-09 06:32:43 +0530

    Update terraform-immutable-app-module/

--- alb.tf
+++ alb.tf
@@ -13,3 +13,32 @@ resource "aws_lb_target_group" "tg" {
     timeout             = 3
   }
 }
+
+resource "aws_lb_listener" "lb-listener" {
+  count             = var.LB_PUBLIC ? 1 : 0
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LB_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "static" {
+  count        = var.LB_PRIVATE ? 1 : 0
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = var.LB_RULE_PRIORITY
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.roboshop.internal"]
+    }
+  }
+}
